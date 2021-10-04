0abc159ef24abd18d99df0b2224c8644c70babfe
FIRST_James LAST_Poda <jamespoda16@gmail.com>
2021-10-04 18:04:17 +0200

    first commit

---
+++ src/components/CourseList/CourseDetail/CourseTabs/Curriculum.js
@@ -0,0 +1,117 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    flexBasis: '33.33%',
+    flexShrink: 0,
+  },
+  secondaryHeading: {
+    fontSize: theme.typography.pxToRem(15),
+    color: theme.palette.text.secondary,
+  },
+}));
+
+export default function Curriculum() {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleChange = (panel) => (event, isExpanded) => {
+    setExpanded(isExpanded ? panel : false);
+  };
+
+  return (
+    <div className={classes.root}>
+      <ExpansionPanel
+        expanded={expanded === 'panel1'}
+        onChange={handleChange('panel1')}
+      >
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel1bh-content"
+          id="panel1bh-header"
+        >
+          <Typography className={classes.heading}>General settings</Typography>
+          <Typography className={classes.secondaryHeading}>
+            I am an expansion panel
+          </Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+            Nothing is complicated. There's something to do with what a lot of
+            people miss.
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel
+        expanded={expanded === 'panel2'}
+        onChange={handleChange('panel2')}
+      >
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel2bh-content"
+          id="panel2bh-header"
+        >
+          <Typography className={classes.heading}>Users</Typography>
+          <Typography className={classes.secondaryHeading}>
+            You are currently not an owner
+          </Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+            Nothing is complicated. There's something to do with what a lot of
+            people miss.
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel
+        expanded={expanded === 'panel3'}
+        onChange={handleChange('panel3')}
+      >
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel3bh-content"
+          id="panel3bh-header"
+        >
+          <Typography className={classes.heading}>Advanced settings</Typography>
+          <Typography className={classes.secondaryHeading}>
+            Filtering has been entirely disabled for whole web server
+          </Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+            Nothing is complicated. There's something to do with what a lot of
+            people miss.
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel
+        expanded={expanded === 'panel4'}
+        onChange={handleChange('panel4')}
+      >
+        <ExpansionPanelSummary
+          expandIcon={<ExpandMoreIcon />}
+          aria-controls="panel4bh-content"
+          id="panel4bh-header"
+        >
+          <Typography className={classes.heading}>Personal data</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+            Nothing is complicated. There's something to do with what a lot of
+            people miss.
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+    </div>
+  );
+}
---
+++ src/components/Navigation/Avatar/AvatarInfo.js
@@ -0,0 +1,112 @@
+import React from "react";
+import cx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Avatar from "@material-ui/core/Avatar";
+import Divider from "@material-ui/core/Divider";
+import { IconButton, Typography, Link, Badge } from "@material-ui/core";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import GitHubIcon from "@material-ui/icons/GitHub";
+
+const useStyles = makeStyles(({ palette }) => ({
+  card: {
+    borderRadius: 12,
+    minWidth: 200,
+    textAlign: "center",
+  },
+  avatar: {
+    width: 60,
+    height: 60,
+    margin: "auto",
+  },
+  heading: {
+    fontSize: 18,
+    fontWeight: "bold",
+    letterSpacing: "0.5px",
+    marginTop: 8,
+    marginBottom: 0,
+  },
+  subheader: {
+    fontSize: 14,
+    color: palette.grey[500],
+    marginBottom: "0.875em",
+  },
+  statLabel: {
+    fontSize: 12,
+    color: palette.grey[500],
+    fontWeight: 500,
+    fontFamily:
+      '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+    margin: 0,
+  },
+  statValue: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 4,
+    letterSpacing: "1px",
+  },
+}));
+
+export const AvatarInfo = React.memo(function ProfileCard() {
+  const styles = useStyles();
+  const user = JSON.parse(localStorage.getItem("user"));
+  const isGV = user && user.maLoaiNguoiDung === "GV";
+
+  return (
+    <Card className={cx(styles.card)}>
+      {user ? (
+        <CardContent>
+          <Badge
+            badgeContent={isGV ? user.maLoaiNguoiDung : null}
+            color="error"
+          >
+            <Avatar
+              className={styles.avatar}
+              src={"https://cdn.pixabay.com/photo/2021/09/11/15/46/witch-6615914_960_720.png"}
+            />
+          </Badge>
+          <h3 className={styles.heading}>{user.taiKhoan}</h3>
+          <span className={styles.subheader}>
+            {user.maNhom}
+            <br />
+            {user.email}
+          </span>
+        </CardContent>
+      ) : null}
+      <Divider light />
+      <Typography className={styles.subheader}>
+        Made with love
+      </Typography>
+      <Typography className={styles.subheader} style={{ marginBottom: 0 }}>
+        Contact me
+      </Typography>
+      <Box display="flex" justifyContent="center">
+        <IconButton
+          size="small"
+          component={Link}
+          href="https://www.facebook.com/james.poda.92/"
+          target="_blank"
+          rel="noopener"
+        >
+          <FacebookIcon />
+        </IconButton>
+        <IconButton
+          size="small"
+          component={Link}
+          href=" https://github.com/podajames"
+          target="_blank"
+          rel="noopener"
+        >
+          <GitHubIcon style={{ fontSize: "1.125rem" }} />
+        </IconButton>
+      </Box>
+      <Typography className={styles.subheader}>
+        © Copyright 2021 Learn Hub.
+      </Typography>
+    </Card>
+  );
+});
+
+export default AvatarInfo;
